fd4acd081e83edf6ffd215b22d338dc0917da3e5
RitanshRajput <it2042@global.org.in>
2024-10-12 01:14:52 +0530

    update-homepage

---
+++ src/component/Productpage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Productpage = () => {
+  return <div>Product Page</div>;
+};
+
+export default Productpage;
---
+++ src/component/Userprofile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Userprofile = () => {
+  return <div>user profile</div>;
+};
+
+export default Userprofile;
