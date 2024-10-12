6dbfd2e2ddbba1de4732438fb40fb625a09cc900
alok-urmaliya <alokurmaliya0123@gmail.com>
2024-10-12 22:56:24 +0530

    Added category API

--- blackwilbur/urls.py
+++ blackwilbur/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from blackwilbur import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('categories', views.CategoryAPIView.as_view()),
 ]
--- blackwilbur/views/auth.py
+++ blackwilbur/views/auth.py
@@ -8,19 +8,17 @@ from rest_framework_simplejwt.tokens import RefreshToken
 class LoginAPIView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
-        
+
         if not serializer.is_valid():
             return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # Authenticate user
+
         email = serializer.validated_data['email']
         password = serializer.validated_data['password']
-        user = authenticate(request, username=email, password=password)  # Assuming email is used as the username
-        
+        user = authenticate(request, username=email, password=password)
+
         if user is None:
             return Response({'error': 'Invalid email or password'}, status=status.HTTP_401_UNAUTHORIZED)
-        
-        # Generate JWT tokens
+
         refresh = RefreshToken.for_user(user)
         
         return Response({
