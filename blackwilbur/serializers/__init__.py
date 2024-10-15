1f5e7437a79eb6614460c350ab53cffa54ad8bf6
yuvraj-singh-lodhi <it2005@global.org.in>
2024-10-15 23:23:46 +0530

    collection api fix

--- blackwilbur/serializers/__init__.py
+++ blackwilbur/serializers/__init__.py
@@ -1,9 +1,7 @@
-from .request_serializers import LoginSerializer, SearchProductSerializer, AddToCartSerializer, EditQuantitySerializer
+from .request_serializers import LoginSerializer, SearchProductSerializer, AddToCartSerializer, EditQuantitySerializer,RegisterSerializer
 from .category import CategorySerializer
 from .product_variation import ProductVariationSerializer
-from .product import ProductSerializer
+from .product import ProductSerializer ,ProductDetailSerializer ,CollectionsSerializer
 from .order import OrderSerializer
 from .cart import CartItemSerializer
 from .product_image import ProductImageSerializer
-from .product import ProductDetailSerializer
-from .request_serializers import RegisterSerializer
--- blackwilbur/serializers/product.py
+++ blackwilbur/serializers/product.py
@@ -8,8 +8,14 @@ class ProductSerializer(ModelSerializer):
 
     class Meta:
         model = models.Product
-        fields = ['id', 'name', 'price', 'product_images']
-        read_only_fields = ['id', 'name', 'price', 'product_images']
+        fields = [
+            'id', 'name', 'description', 'price', 'availability',
+            'discount', 'product_images'
+        ]
+        read_only_fields = [
+            'id', 'name', 'description', 'price', 'availability',
+            'discount', 'product_images'
+        ]
 
     def get_product_images(self, instance):
         images = instance.images.all()
@@ -39,12 +45,59 @@ class ProductDetailSerializer(ModelSerializer):
     def get_rating(self, instance):
         ratings = instance.reviews.all()
         if ratings.exists():
-            average_rating = ratings.aggregate(models.Avg('rating'))['rating__avg']
+            average_rating = ratings.aggregate(
+                models.Avg('rating'))['rating__avg']
         else:
             return 0
         return round(min(max(average_rating, 0), 5), 1)
-    
+
     def get_product_images(self, instance):
         # Fetch images based on the product ID
         images = models.ProductImage.objects.filter(product=instance.id)
         return serializers.ProductImageSerializer(images, many=True).data
+
+
+class CollectionsSerializer(ModelSerializer):
+    sizes = serializers.ProductVariationSerializer(many=True)
+    rating = SerializerMethodField()
+    product_images = SerializerMethodField()
+    category = SerializerMethodField()
+
+    class Meta:
+        model = models.Product
+        fields = ProductSerializer.Meta.fields + [
+            "description",
+            "sizes",
+            "rating",
+            "product_images",
+            "category",
+        ]
+        read_only_fields = ProductSerializer.Meta.fields + [
+            "description",
+            "sizes",
+            "rating",
+            "product_images",
+            "category",
+        ]
+
+    def get_sizes(self, instance):
+        sizes = instance.variations.all()
+        return serializers.ProductVariationSerializer(sizes, many=True).data
+
+    def get_rating(self, instance):
+        ratings = instance.reviews.all()
+        if ratings.exists():
+            average_rating = ratings.aggregate(
+                models.Avg('rating'))['rating__avg']
+        else:
+            return 0
+        return round(min(max(average_rating, 0), 5), 1)
+
+    def get_product_images(self, instance):
+        images = models.ProductImage.objects.filter(product=instance.id)
+        return serializers.ProductImageSerializer(images, many=True).data
+
+    def get_category(self, instance):
+        if instance.category:
+            return serializers.CategorySerializer(instance.category).data
+        return None
