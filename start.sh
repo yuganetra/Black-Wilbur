5218dd466a0c7d892d7f0119cab76fc606f96930
Rupesh Kachhi <90302680+rupesh-kachhi@users.noreply.github.com>
2024-10-21 14:17:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ python manage.py collectstatic --no-input
 # Activate the virtual environment
 source .venv/bin/activate
 
-gunicorn blackwilbur.wsgi:application --bind 0.0.0.0:8000
+gunicorn blackwilbur.wsgi:application
 
